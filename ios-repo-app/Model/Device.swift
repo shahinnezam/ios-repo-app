ac673e54565f6bfc7cd795b63284cec39034a484
Brian Bansenauer <brianb@cascadia.edu>
2019-10-08 11:29:00 -0700

    updates device model with DeviceType setter

--- ios-repo-app/Model/Device.swift
+++ ios-repo-app/Model/Device.swift
@@ -5,12 +5,16 @@
 //  Created by Brian Bansenauer on 10/5/19.
 //  Copyright © 2019 Cascadia College. All rights reserved.
 //
-enum DeviceType:String, Codable{
+enum DeviceType:String, Codable, CaseIterable{
         case iPad = "iPad"
         case iPhone = "iPhone"
         case iPodTouch = "iPod Touch"
         case AndroidPhone = "Android Phone"
         case AndroidTablet = "Android Tablet"
+    
+    static func withLabel(_ label: String) -> DeviceType? {
+        return self.allCases.first{ "\($0)" == label }
+    }
 }
 
 class Device: Codable {
